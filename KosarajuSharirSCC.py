5c03f695901aae283dc9d5ab2b82716036edb025
alphazero-wd <alphazero25811@gmail.com>
2022-06-09 21:19:22 +0700

    fix algorithm name

--- KosarajuSharirSCC.py
+++ KosarajuSharirSCC.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 from TopologicalSort import TopologicalSort
 
-class KorasajuSharirSCC:
+class KosarajuSharirSCC:
   def __init__(self) -> None:
     self.visited = set()
     self.count = 0
