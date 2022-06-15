78be3f76311aa977feb3b1dd2137c6d76569fa1f
alphazero-wd <alphazero25811@gmail.com>
2022-06-15 19:22:30 +0700

    update

--- BST.py
+++ BST.py
@@ -3,63 +3,52 @@ from BinaryTree import BinaryTree, TreeNode
 
 
 class BST(BinaryTree):
-  def __init__(self) -> None:
-    super().__init__()
-  
-  def contains(self, val: int) -> bool:
-    def find(root: Optional[TreeNode]):
-      if not root: return False 
-      if root.val == val: return True
-      elif root.val > val: 
-        return find(root.left)        
-      else: return find(root.right)
-    return find(self.root)
-  
-  def insert(self, val: int) -> None:
-    new_node = TreeNode(val)
-    if not self.root: 
-      self.root = new_node
-      return
-    else:
-      cur = self.root
-      while True:
-        if cur.val > val:
-          if cur.left: cur = cur.left
-          else: 
-            cur.left = new_node
-            break
+    def contains(self, root: Optional[TreeNode], val: int) -> bool:
+        if not root:
+            return
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.contains(root.left, val)
         else:
-          if cur.right: cur = cur.right
-          else: 
-            cur.right = new_node
-            break
-  
-  def delete(self, val: int):
-    def delete_recursively(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-      if not root: return
-      if root.val > val:
-        root.left = delete_recursively(root.left, val)
-      elif root.val < val:
-        root.right = delete_recursively(root.right, val)
-      else:
-        if not root.left and not root.right:
-          return
-        elif not root.left:
-          root = root.right
-          return root
-        elif not root.right:
-          root = root.left
-          return root
-        else:
-          min_node: Optional[TreeNode] = self.find_min(root.right)
-          root.val = min_node.val
-          root.right = delete_recursively(root.right, min_node.val)
-          return root
-      return root
-    return delete_recursively(self.root, val)
+            return self.contains(root.right, val)
 
-  def find_min(self) -> Optional[TreeNode]:
-    cur = self.root                 
-    while cur.left:
-      cur = cur.left
-    return cur
+    def insert(self, root: Optional[TreeNode], val: int) -> None:
+        if not root:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insert(root.left, val)
+        else:
+            root.right = self.insert(root.right, val)
+        return root
+
+    def delete(self, root: Optional[TreeNode], val: int):
+        if not root:
+            return
+        if root.val > val:
+            root.left = self.delete(root.left, val)
+        elif root.val < val:
+            root.right = self.delete(root.right, val)
+        else:
+            # case 1: delete a leaf node
+            if not root.left and not root.right:
+                root = None
+            # case 2: node only has right child
+            elif not root.left:
+                root = root.right
+            # case 3: node only has left child
+            elif not root.right:
+                root = root.left
+            # case 4: node has both left and right children
+            else:
+                min_node = self.find_min(root.right)
+                root.val = min_node.val
+                root.right = self.delete(root.right, min_node.val)
+            return root
+        return root
+
+    def find_min(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        cur = root
+        while cur.left:
+            cur = cur.left
+        return cur
--- BinaryTree.py
+++ BinaryTree.py
@@ -1,46 +1,45 @@
 from typing import Optional
 from queue import Queue
 
+
 class TreeNode:
-  def __init__(self, val: int) -> None:
-    self.val = val
-    self.left: Optional[TreeNode] = None
-    self.right: Optional[TreeNode] = None
-  
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left: Optional[TreeNode] = None
+        self.right: Optional[TreeNode] = None
+
+
 class BinaryTree(object):
-  def __init__(self) -> None:
-    self.root: Optional[TreeNode] = None
+    def inorder(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return
+        self.inorder(root.left)
+        print(root.val, end=' ')
+        self.inorder(root.right)
 
-  def inorder(self) -> None:
-    def traverse(root: Optional[TreeNode]):
-      if not root: return ''
-      traverse(root.left)
-      print(root.val, end=' ')
-      traverse(root.right)
-    traverse(self.root)
-  
-  def preorder(self) -> None:
-    def traverse(root: Optional[TreeNode]):
-      if not root: return ''
-      print(root.val, end=' ')
-      traverse(root.left)
-      traverse(root.right)
-    traverse(self.root)
+    def preorder(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return
+        print(root.val, end=' ')
+        self.preorder(root.left)
+        self.preorder(root.right)
 
-  def postorder(self) -> None:
-    def traverse(root: Optional[TreeNode]):
-      if not root: return ''
-      traverse(root.left)
-      traverse(root.right)
-      print(root.val, end=' ')
-    traverse(self.root)
-  
-  def level_order(self) -> None:
-    if not self.root: return
-    queue: Queue[TreeNode] = Queue()
-    queue.put(self.root)
-    while not queue.empty():
-      cur = queue.get()
-      print(cur.val, end=" ")
-      if cur.left: queue.put(cur.left)
-      if cur.right: queue.put(cur.right)
+    def postorder(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return ''
+        self.postorder(root.left)
+        self.postorder(root.right)
+        print(root.val, end=' ')
+
+    def level_order(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return
+        queue: Queue[TreeNode] = Queue()
+        queue.put(root)
+        while not queue.empty():
+            cur = queue.get()
+            print(cur.val, end=" ")
+            if cur.left:
+                queue.put(cur.left)
+            if cur.right:
+                queue.put(cur.right)
